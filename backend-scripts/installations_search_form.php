b5abcb7d92d008252f7f209bd966bd5084276111
yenlingwong <74548407+yenlingwong@users.noreply.github.com>
2022-03-09 12:42:43 +0100

    Update installations_search_form.php

--- backend-scripts/installations_search_form.php
+++ backend-scripts/installations_search_form.php
@@ -11,7 +11,26 @@
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
     <title>Kronos Games</title>
+
+    <style type="text/css">
+      .ui-autocomplete-row
+      {
+        padding:8px;
+        background-color: #f4f4f4;
+        border-bottom:1px solid #ccc;
+        font-weight:bold;
+      }
+      .ui-autocomplete-row:hover
+      {
+        background-color: #ddd;
+      }
+    </style>
+
 </head>
  
 <body>
@@ -50,11 +69,31 @@
     </div>
         <form action="installations_search_form.php" method="POST">
             <h1 style="color:coral">Enter graphics card: </h1>
-            <input type="text" required="required" name="installations_search" placeholder="Search">
+            <input type="text" required="required" name="installations_search" placeholder="Search" id="isearch">
             <button type="submit" required="required" name="submit-search">Search</button>
         </form>
         
         <div class="installations-result-container">
+
+        <script>
+            $(document).ready(function(){
+                $('#isearch').autocomplete({
+                source: "installations_fetch_data.php",
+                minLength: 1,
+                select: function(event, ui)
+                {
+                    $('#isearch').val(ui.item.value);
+                }
+                }).data('ui-autocomplete')._renderItem = function(ul, item){
+                return $("<li class='ui-autocomplete-row'></li>")
+                    .data("item.autocomplete", item)
+                    .append(item.label)
+                    .appendTo(ul);
+                };
+            });
+        </script>
+
+
 <?php
     if(isset($_POST['submit-search'])) {
         $search = mysqli_real_escape_string($conn, $_POST['installations_search']);
